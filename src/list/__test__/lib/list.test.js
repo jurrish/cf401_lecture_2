91ae5aa6199ff5d78362b479eff98e40b98af654
Frank Iriarte <jurrish@For-the-screen-is-dark-and-full-of-errors.local>
2020-02-08 17:41:49 -0800

    javascript List and array functions, along with Class wrapper and tests

--- src/list/__test__/lib/list.test.js
+++ src/list/__test__/lib/list.test.js
@@ -4,14 +4,49 @@ const List = require('../../lib/list.js');
 
 describe('Testing the list class', () => {
   it('should have a length of zero', () => {
-
+    let initialList = new List();
+    expect(initialList.length).toBe(0);
   })
 
   it('should add a new element to the List', () => {
-
+    let pushList = new List();
+    pushList.push(6);
+    pushList.push(8);
+    pushList.push(1);
+    pushList.push(3);
+    expect(pushList.length).toBe(4);
   })
 
-  it('should iterate over the array and run the callback for each element', () => {
+  it('should iterate over the array and run the callback for each element and return a new list of modified values', () => {
+    let mapList = new List();
+    mapList.push(5);
+    mapList.push(4);
+    mapList.push(9);
+    mapList.push(16);
 
+    //takes in callback that performs operations on each element
+    let actual = mapList.map(num => num * 2);
+    console.log(actual);
+
+    expect(actual.length).toEqual(mapList.length);
+    expect(actual).not.toEqual(mapList);
+    expect(actual[0]).toEqual(10);
+  })
+})
+
+
+describe('testing the pop method', () => {
+  it('should remove the last element from the list', () => {
+    let popList = new List();
+    let last = popList[popList.length - 1];
+    let popped = popList.pop();
+    expect(popped).toBe(last);
+  })
+
+  it('should reduce length by one', () => {
+    let wat = new List();
+    wat.push(5);
+    wat.pop();
+    expect(wat.length).toBe(0);
   })
 })
--- src/list/lib/list.js
+++ src/list/lib/list.js
@@ -4,6 +4,68 @@ class List {
   constructor() {
     this.length = 0;
   }
+
+  push(value) {
+    //add value to back of list and assign index
+    this[this.length] = value;
+
+    //increment length
+    this.length++
+
+    //return the length
+    return this.length;
+  }
+
+  map(callback) {
+    let newList = new List();
+
+    //iterate over array of values
+    for(let i = 0; i < this.length; i++){
+      //perform some operation on it
+     newList.push( callback( this[i] ) );
+    }
+    return newList;
+  }
+
+  pop() {
+    let popped = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+    return popped;
+  }
+
+  filter(callback) {
+    let newList = new List();
+    for(let i = 0; i < this.length; i++) {
+      //if truthy, push value
+      if(callback( this[i] ))
+      {
+        newList.push( this[i] );
+      }
+    }
+    return newList;
+  }
+
+  reduce(callback, initialValue) {
+    let accumulate;
+
+    for (var i = 0; i < this.length - 1; i++) {
+      accumulate = callback(initialValue, this[i], i);
+    }
+    return accumulate;
+  }
+
+  slice(start, end) {
+    let newList = new List();
+    let startingIndex = 0;
+    for (var i = start; i <= end; i++) {
+      newList[i - start] = this[i];
+      this.length--;
+    }
+    return newList;
+  }
+
+
 }
 
 module.exports = List;
